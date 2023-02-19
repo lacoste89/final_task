40533972e1432a9a3bedfe32a6ab4306fa586881
Renat <f@f.ru>
2023-02-19 14:29:45 +0300

    add programm

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Напишите программу, которая из имеющегося массивы строк формирует массив из строк,
+длина которых меньше либо ровнаа 3 символам.
+*/
+
+using static System.Console;
+Clear();
+
+Write("Введите слова через пробел: ");
+string[] str_array = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+WriteLine($"[{String.Join(", ", str_array)}]");
+
+string[] str_array2 = filter_array_str(str_array);
+WriteLine($"[{String.Join(", ", str_array2)}]");
+
+string[] filter_array_str(string[] in_array){
+    string temp = "";
+
+    for (int i = 0; i < in_array.Length; i++)
+    {
+        if(in_array[i].Length <= 3) temp += $"{in_array[i]} ";
+    }
+
+    return temp.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+}
+
